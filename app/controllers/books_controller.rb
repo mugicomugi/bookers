f55dac953f320568036a71efbb1f3af72d1ca715
EC2 Default User <127326435+mugicomugi@users.noreply.github.com>
2023-04-01 13:47:49 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,19 +27,27 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
-     flash[:notice] = "Book was successfully edited."
+    
+     
+     
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully edited."
+       redirect_to book_path(@book)
+       
+    else
+      @books = Book.all
+       render :edit
+    end
   end
   
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    @book = Book.all
+    redirect_to books_path
   end
 
   private
